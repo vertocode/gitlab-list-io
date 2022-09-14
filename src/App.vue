18ab336f434e3d603a763cc8e47e419a51e777b4
vertocode <evertonvanoni1@gmail.com>
2022-09-13 22:37:32 -0300

    feat: navbar finished

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <NavBar />
+    <router-view />
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import NavBar from '@/components/NavBar'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  data: () => ({
+    //
+  }),
+  components: {
+    NavBar
   }
 }
-</style>
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="navbar">
+    <header>
+      <div class="img-container">
+        <img src="../assets/gitlab.webp" alt="GitLab" height="100px">
+      </div>
+    </header>
+    <main>
+      <ul>
+        <li v-for="option in optionsNav" :key="option.name" @click="updateCurrentRoute(option.route)">
+          <router-link
+            :style="option.route === currentRoute ? 'text-decoration: underline;' : ''"
+            :to="option.route"
+          >
+            {{ option.name }}
+          </router-link>
+        </li>
+      </ul>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      currentRoute: window.location.pathname,
+      optionsNav: [
+        {
+          name: 'Register',
+          route: '/'
+        },
+        {
+          name: 'Project List',
+          route: '/list'
+        }
+      ]
+    }
+  },
+  methods: {
+    updateCurrentRoute (route) {
+      console.log(route)
+      this.currentRoute = route
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.navbar {
+  background-color: aliceblue;
+  header {
+    display: flex;
+    .img-container {
+      margin: auto;
+      img {
+        margin-left: 1rem
+      }
+    }
+  }
+  main {
+    width: 100%;
+    display: flex;
+  }
+  ul {
+    margin: 20px auto;
+    a {
+      margin: auto 1rem auto 2rem;
+      text-decoration: none;
+      font-weight: bold;
+      font-family: "Droid Sans Mono",sans-serif;
+      font-size: 1.5rem;
+    }
+    li {
+      float: left;
+      list-style: none;
+      margin: auto 20px;
+      a {
+        font-family: "Droid Sans Mono", serif;
+        color: black;
+      }
+    }
+  }
+}
+
+</style>
